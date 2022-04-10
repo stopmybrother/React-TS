d2d0d04b6165a77293cedc4aea43944d86fcf6b5
Zhenya Aleksandrova <laberfo@gmail.com>
2022-04-10 03:47:06 +0300

    fix: styled-components, props

--- react-ts-hw-all/src/Button/Button.tsx
+++ react-ts-hw-all/src/Button/Button.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+
 import styled, { css } from "styled-components";
+
 import { TButtonComponentStyle, TLink } from './button-types'
 
 interface IBtnProps {
@@ -10,91 +12,97 @@ interface IBtnProps {
     href?: string;
     onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
     type: TButtonComponentStyle;
+    borderRadius?: number;
 };
 
-const StyledButton = styled.button`
-    display: block;
-    margin: 0 auto 100px;
+const Container = styled.div`
+  margin: 0 auto 50px;
+  padding: 0 16px;
+`;
+
+const Btn = styled.button<IBtnProps>`
+    margin: 0 auto;
     padding: 10px 50px;
-    border: 1px solid #000;
-    border-radius: 10px;
-    background-color: rgb(84, 176, 219);
-    color: rgba($color: #000000, $alpha: .5);
+    border: 1px solid ${ p => p.theme.colors.borderColor };
+    border-radius: ${p => p.borderRadius}px;
+    background-color: ${ p => p.theme.colors.primaryBlue };
+    color: ${ p => p.theme.colors.translucentBlack };
     font-size: 32px;
     line-height: 28px;
-    text-transform: uppercase;
-    cursor: pointer;
+
     transition: 0.3s linear;
     
     &:hover:enabled {
-        background-color: rgb(137, 193, 219);
+        background-color: ${ p => p.theme.colors.hoverBlue };
+        color: ${ p => p.theme.colors.black };
     }
     &:active:enabled {
         transition: 0s linear;
-        background-color: rgb(77, 174, 219);
+        background-color: ${ p => p.theme.colors.activeBlue };
+        color: ${ p => p.theme.colors.black };
     }
 
     &.active {
-        background-color: rgb(66, 201, 95);
+        background-color: ${ p => p.theme.colors.primaryGreen };
     }
     &.active:hover:enabled {
-        background-color: rgb(110, 199, 130);
+        background-color: ${ p => p.theme.colors.hoverGreen };
+        color: ${ p => p.theme.colors.black };
     }
     &.active:active:enabled {
         transition: 0s linear;
-        background-color: rgb(19, 194, 57);
+        background-color: ${ p => p.theme.colors.activeGreen };
+        color: ${ p => p.theme.colors.black };
     }
 
     ${props => props.disabled && css`
-        cursor: default;
-        background-color: rgb(181, 201, 211);
+        background-color: ${ p => p.theme.colors.disabledBlue };
     `}
-`
-const StyledLink = styled.a`
-    display: block;
+`;
+
+const Link = styled.a`
     margin: 0 auto 50px;
-    cursor: pointer;
     font-size: 32px;
     line-height: 28px;
-    text-decoration: none;
-    color: rgb(48, 135, 185);
+    color: ${ p => p.theme.colors.primaryBlue };
     transition: 0.3s linear;
     
     &:hover {
-        color: rgb(77, 146, 185);
+        color: ${ p => p.theme.colors.hoverBlue };
     }
     &:active {
         transition: 0s linear;
-        color: rgb(9, 118, 182);
+        color: ${ p => p.theme.colors.activeBlue };
     }
 
     &.active {
-        color: rgb(50, 206, 84);
+        color: ${ p => p.theme.colors.primaryGreen };
     }
     &.active:hover {
-        color: rgb(96, 204, 120);
+        color: ${ p => p.theme.colors.hoverGreen };
     }
     &.active:active {
         transition: 0s linear;
-        color: rgb(4, 196, 45);
+        color: ${ p => p.theme.colors.activeGreen };
     }
-`
+`;
+
 export const Button = ( {
                             className = "",
                             children = "Default button",
                             active = false,
                             disabled = false,
-                            onClick = function (e: React.MouseEvent<HTMLButtonElement>) {},
+                            onClick = () => {},
                             href = "#",
                             type = "button",
                         }: IBtnProps ) => {
 
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (disabled) {
+    const handleClick = ( e: React.MouseEvent<HTMLButtonElement> ) => {
+        if ( disabled ) {
             e.preventDefault();
         } else {
-            console.log("Click");
-            return onClick(e);
+            console.log( "Click" );
+            return onClick( e );
         }
     };
 
@@ -102,30 +110,23 @@ export const Button = ( {
 
     return (
         <>
-            {isLink ? (
-                <StyledLink href={href} className={`${className} ${active ? "active" : ""}`}>
-                    {children}
-                </StyledLink>
-            ) : (
-                <StyledButton
-                    disabled={disabled}
-                    onClick={handleClick}
-                    className={`${className} ${active ? "active" : ""}`}
-                    type={type as Exclude<TButtonComponentStyle, TLink>}
-                >
-                    {children}
-                </StyledButton>
-            )}
+            <Container>
+                {isLink ? (
+                    <Link href = { href } className = { `${className} ${active ? "active" : ""}` }>
+                        { children }
+                    </Link>
+                ) : (
+                    <Btn
+                        disabled = { disabled }
+                        onClick = { handleClick }
+                        className = { `${className} ${active ? "active" : ""}` }
+                        type = { type as Exclude<TButtonComponentStyle, TLink> }
+                        borderRadius = { 10 }
+                    >
+                        { children }
+                    </Btn>
+                )};
+            </Container>
         </>
     );
-}
-
-// Создать кастомный компонент кнопки (Button), который может принимать в себя следующие пропсы
-// disabled - становится бледной (boolean, default: false)
-// onClick - обработчик события при нажатии - выполняется при нажатии кнопки (event, default: () => {})
-// children - содержимое кнопки (текст и/или иконки внутри) (node, default: 'Default button')
-// active - активное состояние (boolean, default: false)
-// className (string, default: '')
-// type (“button” | “link” | “submit”) (string, default: “button”)
-// href - опциональный, применяется только при type = “link”
-// При hover кнопка должна плавно менять цвет, при нажатии - резко (цвета на ваше усмотрение)
+};
