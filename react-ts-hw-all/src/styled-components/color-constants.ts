b8a995aaf0774873f0c057cff8b97467fdc62ece
Zhenya Aleksandrova <laberfo@gmail.com>
2022-04-10 03:38:30 +0300

    feature: added styled-components/ - themes & color-constants

---
+++ react-ts-hw-all/src/styled-components/color-constants.ts
@@ -0,0 +1,25 @@
+export const COLOR: Record<string, string> = {
+    black: "#000",
+    translucentBlack: "#0000007F",
+
+    white: "#FFF",
+
+    primaryBlue: "#53A3D7FF",
+    hoverBlue: "#94C9DEFF",
+    activeBlue: "#33A9DEFF",
+
+    magicMint: "#AAF1DAFF",
+
+    primaryGreen: "#42C95FFF",
+    hoverGreen: "#6EC782FF",
+    activeGreen: "#13C239FF",
+
+    primaryPink: "#D666D8FF",
+    hoverPink: "#DE8AE0FF",
+    activePink: "#DA3EDDFF",
+
+    primaryOrange: "#EFA55BFF",
+    hoverOrange: "#e7b582",
+    activeOrange: "#ec841e",
+    disabledOrange: "#817d79",
+}
---
+++ react-ts-hw-all/src/styled-components/themes.ts
@@ -0,0 +1,41 @@
+export const themes = {
+    colors: {
+        black: "#000",
+        translucentBlack: "#0000007F",
+
+        white: "#FFF",
+
+        primaryBlue: "#53A3D7FF",
+        hoverBlue: "#94C9DEFF",
+        activeBlue: "#33A9DEFF",
+        disabledBlue: "#B5C9D3FF",
+
+        magicMint: "#AAF1DAFF",
+
+        primaryGreen: "#42C95FFF",
+        hoverGreen: "#6EC782FF",
+        activeGreen: "#13C239FF",
+
+        primaryPink: "#D666D8FF",
+        hoverPink: "#DE8AE0FF",
+        activePink: "#DA3EDDFF",
+
+        primaryOrange: "#EFA55BFF",
+        hoverOrange: "#e7b582",
+        activeOrange: "#ec841e",
+        disabledOrange: "#817d79",
+
+        primaryYellow: "#e3ea47",
+        lightYellow: "#E6EA7DFF",
+
+        lightGray: "#777777",
+    },
+
+    media: {
+        phone: '(max-width: 600px)',
+        tabletPortrait: '(max-width: 900px)',
+        tabletLandscape: '(max-width: 1200px)',
+        desktop: '(max-width: 1800)',
+        bigDesktop: '(max-width: 2400px)',
+    }
+}
