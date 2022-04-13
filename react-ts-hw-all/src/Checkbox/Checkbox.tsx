71dfea0932578fc9cad272aa6689b88f4150eb46
Zhenya Aleksandrova <laberfo@gmail.com>
2022-04-14 00:43:24 +0300

    fix: did custom-checkbox in styled-components

--- react-ts-hw-all/src/Checkbox/Checkbox.tsx
+++ react-ts-hw-all/src/Checkbox/Checkbox.tsx
@@ -1,137 +1,109 @@
 import React from "react";
-// import { animated, useSpring } from "react-spring"
 
-import "./Checkbox.css";
+import styled from "styled-components";
+
 import { useState } from "react";
 
-export const Checkbox = () => {
+interface ICheckboxComponent {
+    type: string;
+};
+
+interface IInputCheckboxTagProps {
+    type: string;
+    onChange: (e: React.ChangeEventHandler<HTMLInputElement>) => void;
+};
+
+interface ISvgPseudoCheckboxProps {
+    className: string;
+    ariaHidden: boolean;
+    viewBox: string;
+};
+
+interface ISvgPathProps {
+    d: string
+    stroke: string;
+};
+
+const Container = styled.div`
+  margin: 0 auto 50px;
+  padding: 0 16px;
+`;
+
+const Label = styled.label`
+  position: relative;
+  display: flex;
+  align-items: center;
+  max-width: 200px;
+  width: 100%;
+`;
+
+const HiddenCheckbox = styled.input<IInputCheckboxTagProps>`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  clip: rect( 0 0 0 0 );
+  clip-path: inset( 50% );
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+const WatchedCheckbox = styled.svg<ISvgPseudoCheckboxProps>`
+  display: inline-block;
+  
+  margin-right: 7px;
+  width: 20px;
+  height: 20px;
+  background-color: ${ p => p.theme.colors.lightYellow };
+  border: 2px solid ${ p => p.theme.colors.lightGray };
+  border-radius: 4px;
+  fill: none;
+  
+  &:not(:disabled):not(:checked):hover {
+    border-color: ${ p => p.theme.colors.secondaryGray };
+    background-color: ${ p => p.theme.colors.secondaryYellow };
+  }
+  
+  &.active {
+    box-shadow: 0 0 6px 3px ${ p => p.theme.colors.translucentBlackThirty };
+    border-color: ${ p => p.theme.colors.primaryGray };
+    background-color: ${ p => p.theme.colors.primaryYellow };
+  }
+`;
+
+const Path = styled.path<ISvgPathProps>`
+  stroke-width: 2;
+`;
+
+
+export const Checkbox = ({
+                            type = "",
+                         }: ICheckboxComponent) => {
+
     const [isChecked, setIsChecked] = useState( false );
-    // const checkboxAnimationStyle = useSpring( {
-    //     backgroundColor: isChecked ? "#808" : "#fff",
-    //     borderColor: isChecked ? "#808" : "fff",
-    // } )
 
     return (
-        <label>
-            <input
-                type = "checkbox"
-                onChange = {
-                    () => {
-                        setIsChecked( !isChecked );
+        <Container>
+            <Label>
+                <HiddenCheckbox
+                    type = { type }
+                    onChange = {
+                        () => {
+                            setIsChecked( !isChecked );
+                        }
                     }
-                }
-            />
-            <svg
-                // style = { checkboxAnimationStyle }
-                className = { `checkbox ${ isChecked ? "checkbox--active" : "" }` }
-                aria-hidden = { true }
-                viewBox = "0 0 15 11"
-                fill = "none"
-            >
-                <path
-                    d = "M1 4.5L5 9L14 1"
-                    strokeWidth ={ 2 }
-                    stroke = { isChecked? "#fff" : "none" }
                 />
-            </svg>
-            bla bla
-        </label>
+                    <WatchedCheckbox
+                        className = { `checkbox ${ isChecked ? "active" : "" }` }
+                        ariaHidden = { true }
+                        viewBox = "0 0 15 11"
+                    >
+                        <Path
+                            d = "M1 4.5L5 9L14 1"
+                            stroke = { isChecked ? "#383835" : "none" }
+                        />
+                    </WatchedCheckbox>
+                Lucky checkbox
+            </Label>
+        </Container>
     )
-}
-
-// import { useState } from "react";
-// import styled, { keyframes } from "styled-components";
-//
-// interface ICheckbox {
-//     type: string;
-//     checked: boolean;
-//     disabled: boolean;
-//     id?: string;
-//     // onChange: (e: React.MouseEvent<HTMLFormElement>) => void;
-// }
-//
-// const Container = styled.div`
-//   margin: 0 auto;
-//   padding: 0 16px;
-// `
-//
-// const Icon = styled.svg`
-//   fill: none;
-//   stroke: ${ p => p.theme.colors.white };
-//   stroke-width: 2px;
-// `
-//
-// const HiddenCheckbox = styled.input.attrs( { type: "checkbox" } )<ICheckbox>`
-//   margin: -1px;
-//   padding: 0;
-//   width: 1px;
-//   height: 1px;
-//   position: absolute;
-//   clip: rect(0 0 0 0);
-//   clip-path: inset(50%);
-//   overflow: hidden;
-//   white-space: nowrap;
-//   border: 0;
-// `;
-//
-// const InputCheckbox = styled.input.attrs( { type: "checkbox" } )<ICheckbox>`
-//   display: inline-block;
-//   width: 16px;
-//   height: 16px;
-//   background-color: ${ p => p.checked ? p.theme.colors.primaryYellow : p.theme.colors.lightYellow };
-//   border-radius: 3px;
-//   transition: all 0.15s linear;
-//
-//   ${ HiddenCheckbox }:focus + & {
-//     box-shadow: 0 0 0 3px ${ p => p.theme.colors.lightGray };
-//   }
-//
-//   ${ Icon } {
-//     visibility: ${ p => p.checked ? "visible" : "hidden" };
-//   }
-// `;
-//
-// const Label = styled.label
-// const Span = styled.span`
-//   margin-left: 8px;
-// `
-//
-// export const Checkbox = ({
-//                              type = "",
-//                              checked = false,
-//                              disabled = false,
-//                              id = ""
-//                              // onChange = () => {},
-//                          }: ICheckbox) => {
-//
-//     // const changeStateCheckbox = (e: React.MouseEvent<HTMLFormElement>) => {
-//     //     if ( disabled ) {
-//     //         e.preventDefault();
-//     //     } else {
-//     //         console.log( "Click" );
-//     //         return onChange( e );
-//     //     }
-//     // }
-//
-//     return (
-//         <>
-//             <Container>
-//                 <Label for = { id }>
-//                     <HiddenCheckbox type = { type } checked = { checked } disabled = { disabled } />
-//                     <InputCheckbox
-//                         type = { type }
-//                         checked = { checked }
-//                         disabled = { disabled }
-//                         id = {id}
-//                         // onChange = { changeStateCheckbox }
-//                     >
-//                         <Icon viewBox = "0 0 24 24">
-//                             <polyline points = "20 6 9 17 4 12"/>
-//                         </Icon>
-//                     </InputCheckbox>
-//                 </Label>
-//             </Container>
-//         </>
-//     )
-// }
-
+};
