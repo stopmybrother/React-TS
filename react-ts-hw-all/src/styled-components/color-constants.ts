44f6001de3b869f86b2eb9e4b210b98ab3916213
Zhenya Aleksandrova <laberfo@gmail.com>
2022-04-14 00:41:33 +0300

    fix: added colors for checkbox

--- react-ts-hw-all/src/styled-components/color-constants.ts
+++ react-ts-hw-all/src/styled-components/color-constants.ts
@@ -1,12 +1,18 @@
 export const COLOR: Record<string, string> = {
     black: "#000",
     translucentBlack: "#0000007F",
+    translucentBlackThirty: "#0000004C",
+
+    primaryGray: "#333330",
+    secondaryGray: "#50504b",
+    lightGray: "#7c7c6e",
 
     white: "#FFF",
 
     primaryBlue: "#53A3D7FF",
     hoverBlue: "#94C9DEFF",
     activeBlue: "#33A9DEFF",
+    disabledBlue: "#B5C9D3FF",
 
     magicMint: "#AAF1DAFF",
 
@@ -22,4 +28,8 @@ export const COLOR: Record<string, string> = {
     hoverOrange: "#e7b582",
     activeOrange: "#ec841e",
     disabledOrange: "#817d79",
+
+    primaryYellow: "#eee31d",
+    secondaryYellow: "#eee353",
+    lightYellow: "#f3ee91",
 }
