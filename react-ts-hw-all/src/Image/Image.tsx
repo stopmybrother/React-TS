9d7d995acf18b06cd81e3aa71198b3ef65680e5c
Zhenya Aleksandrova <laberfo@gmail.com>
2022-04-14 17:39:29 +0300

    fix: remove extra spaces & now it works with your help :) thanks

--- react-ts-hw-all/src/Image/Image.tsx
+++ react-ts-hw-all/src/Image/Image.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styled from "styled-components";
 
 interface IImageTagProps {
@@ -50,8 +49,7 @@ export const Image = ({
                 src = { src }
                 width = { width }
             >
-
             </Img>
         </Container>
-    )
+    );
 };
