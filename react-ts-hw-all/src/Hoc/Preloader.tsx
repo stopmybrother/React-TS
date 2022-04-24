2af9ea8925b48598ebf40cf493413abe09f36199
Zhenya Aleksandrova <laberfo@gmail.com>
2022-04-24 18:29:26 +0300

    fix: get fetch on button-click in Hoc/Preloader.tsx

--- react-ts-hw-all/src/Hoc/Preloader.tsx
+++ react-ts-hw-all/src/Hoc/Preloader.tsx
@@ -1,12 +1,19 @@
-import React, { useState, useEffect } from "react";
-import axios, { AxiosResponse } from "axios";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { LoaderHOC } from "./LoaderHOC/LoaderHoc";
-import { CommonContainer, CommonContainerFlexBox } from "../styled-components/common-components/wrappers-and-containers/CommonContainer";
+import {
+    CommonContainer,
+    CommonContainerFlexBox
+} from "../styled-components/common-components/wrappers-and-containers/CommonContainer";
 
 interface IAppComponentUIProps {
     readonly data: Record<string, any>;
-};
+    isLoading?: boolean;
+}
+
+interface IP {
+    display: string;
+}
 
 const Container = styled(CommonContainerFlexBox)`
   padding-top: 5px;
@@ -17,20 +24,20 @@ const Container = styled(CommonContainerFlexBox)`
   align-content: center;
   justify-content: space-between;
   column-gap: 20px;
-  background-color: ${ p => p.theme.colors.darkBlue };
-  border: 1px solid ${ p => p.theme.colors.paleDarkBlue };
+  background-color: ${(p) => p.theme.colors.darkBlue};
+  border: 1px solid ${(p) => p.theme.colors.paleDarkBlue};
   border-radius: 10px;
-  box-shadow: 0 0 10px 3px ${ p => p.theme.colors.veryPaleDarkBlue };
+  box-shadow: 0 0 10px 3px ${(p) => p.theme.colors.veryPaleDarkBlue};
 `;
 
 const Button = styled.button`
   font-size: 25px;
-  color: ${ p => p.theme.colors.brightMint };
+  color: ${(p) => p.theme.colors.brightMint};
   padding: 20px 50px;
   background-color: transparent;
-  border: 3px solid ${ p => p.theme.colors.brightMint };
+  border: 3px solid ${(p) => p.theme.colors.brightMint};
   position: relative;
-  transition: all .5s linear;
+  transition: all 0.5s linear;
 
   &::before,
   &::after {
@@ -60,76 +67,71 @@ const Button = styled.button`
   }
 
   &:active {
-    color: ${ p => p.theme.colors.lightMint };
-    border-color: ${ p => p.theme.colors.veryLightMint };
+    color: ${(p) => p.theme.colors.lightMint};
+    border-color: ${(p) => p.theme.colors.veryLightMint};
   }
-  
-  ${ p => p.disabled && css`
-    transition: all .2s linear;
-    color: #858888;
-    border-color: #868888;
-  ` }
+
+  ${(p) =>
+    p.disabled &&
+    css`
+      transition: all 0.2s linear;
+      color: #858888;
+      border-color: #868888;
+    `}
 `;
 
-const P = styled.p`
+const P = styled.p<IP>`
   padding: 20px 8px;
   max-width: 200px;
   width: 100%;
   min-height: 70px;
-  border: 1px solid ${ p => p.theme.colors.purple };
+  border: 1px solid ${(p) => p.theme.colors.purple};
   border-radius: 10px;
-  background-color: ${ p => p.theme.colors.paleMint };
-  color: ${ p => p.theme.colors.veryDarkPurple };
+  background-color: ${(p) => p.theme.colors.paleMint};
+  color: ${(p) => p.theme.colors.veryDarkPurple};
   text-transform: uppercase;
   text-align: right;
+  display: ${(p) => p.display};
 `;
 
-const AppComponentUI = ( props: IAppComponentUIProps ) => {
+const AppComponentUI = (props: IAppComponentUIProps) => {
     return (
-            <P>
-                { props?.data.data.title }
-            </P>
+        <P display={props?.data.title ? "block" : "none"}>{props?.data.title}</P>
     );
 };
 
-const AppComponent = LoaderHOC( "data" )( AppComponentUI );
+const AppComponent = LoaderHOC("isLoading")(AppComponentUI);
 
 const GetResponse = () => {
-    const [ post, setPost ] = useState( {} );
-    // const [ loading, setLoading ] = useState( false ); // wanted to send a request on button-click (don't know how to transfer loading-state in HOC)
+    const [post, setPost] = useState({});
+    const [loading, setLoading] = useState(false); // wanted to send a request on button-click (don't know how to transfer loading-state in HOC)
 
-    useEffect( () => {
-        axios
-            .get<AxiosResponse>( "https://jsonplaceholder.typicode.com/posts/1" )
-            .then( (data) => {
-                setTimeout( () => {
-                    // setLoading( false );
-                    setPost( data );
-                }, 5000);
-            })
-            .catch( ( error ) => {
-                // setLoading( false );
-                console.log( error );
-            });
-    }, []);
+    const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setLoading(true);
+        setTimeout(async () => {
+            try {
+                const getFetch = await fetch(
+                    "https://jsonplaceholder.typicode.com/posts/1"
+                );
+                const json = await getFetch.json();
+                setLoading(false);
+                setPost(json);
+            } catch (error) {
+                console.log(error);
+            }
+        }, 5000);
+    };
 
     return (
         <>
-            <CommonContainer
-                marginBottom = { 50 }
-                paddingRight = { 16 }
-                paddingLeft = { 16 }
-            >
+            <CommonContainer>
                 <Container>
-                    <Button
-                        // disabled = { loading }
-                        // onClick = { () => { setLoading( true ) } }
-                        // type = "button"
-                    >
+                    <Button disabled={loading} onClick={handleClick} type="button">
                         Get data
                     </Button>
 
-                    <AppComponent data = { post } />
+                    <AppComponent data={post} isLoading={loading} />
                 </Container>
             </CommonContainer>
         </>
