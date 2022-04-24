b1511bb231377ea41d4122d8216941cb00119401
Zhenya Aleksandrova <laberfo@gmail.com>
2022-04-24 18:37:54 +0300

    fix: nothing changed, just was trying to make validation funk in Input/Validation.ts

--- react-ts-hw-all/src/Input/Validation.ts
+++ react-ts-hw-all/src/Input/Validation.ts
@@ -1,17 +1,29 @@
 import React from "react";
 
-const validators = [
+interface IValidationObject {
+    validationFunk?: () => void;
+    error: string;
+};
+
+const getValidate = () => {
+
+};
+
+const validators: IValidationObject[] = [
     {
-        validationFunc: "… (проверка, чтобы введенных символов было минимум 2)",
-        error: "Ошибка, должно быть введено не менее 2 символов",
+        // validationFunc: "… (проверка, чтобы введенных символов было минимум 2)",
+        error: "Error. Here should be more than 2 symbols entered",
     },
     {
-        validationFunc: "… (проверка чтобы поле не было пустым)",
-        error: "Ошибка, необходимо ввести данные",
+        // validationFunc: "… (проверка чтобы поле не было пустым)",
+        error: "Error. You didn't complete this field",
     },
-]
+];
 
 // Создать кастомный компонент для поля ввода (Input) , который может принимать в себя следующие пропсы:
+//      id - id для input и соответствующего ему label (string, default: ‘’),
+//      className - класс для стилизации (string, default: ''),
+//      label - label для нашего input (string, default: ''),
 //      validation - массив объектов вида:
 //          { validationFunc, error }
 //              validationFunc - функция, по которой можно проводить валидацию
@@ -21,7 +33,12 @@ const validators = [
 
 // То есть мы можем сделать массив вида:
 //     const validators = [
-//         { validationFunc: … (проверка, чтобы введенных символов было минимум 2),
-//         error: “Ошибка, должно быть введено не менее 2 символов” },
-//         { validationFunc: … (проверка чтобы поле не было пустым),
-//         error: “Ошибка, необходимо ввести данные” }
+//         {
+//              validationFunc: … (проверка, чтобы введенных символов было минимум 2),
+//              error: “Ошибка, должно быть введено не менее 2 символов”
+//          },
+//         {
+//              validationFunc: … (проверка чтобы поле не было пустым),
+//              error: “Ошибка, необходимо ввести данные”
+//          }
+//      ]
