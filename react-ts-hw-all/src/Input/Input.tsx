0e8d0e6ca5f72ad128d08804ab16d4696394bfa6
Zhenya Aleksandrova <laberfo@gmail.com>
2022-04-24 18:36:34 +0300

    fix: added styles, form and useState for onChange in input in Input/Input.tsx

--- react-ts-hw-all/src/Input/Input.tsx
+++ react-ts-hw-all/src/Input/Input.tsx
@@ -1,22 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { CommonContainer } from "../styled-components/common-components/wrappers-and-containers/CommonContainer";
 
+interface IForm {
+    for?: number;
+}
 
+interface IInput {
+    id?: number,
+    label?: string,
+    validation?: object[],
+    onChange?: (e: React.FormEvent<HTMLInputElement>) => void,
+    onKeyPress?: (e: React.KeyboardEvent<HTMLInputElement>) => void,
+    value?: string,
+    placeholder?: string;
+}
 
-export const Input = ({
-                          id = "",
-                          label = "",
-                          validation = []
-                      }) => {
+const FormTag = styled.form<IForm>`
+  margin: 0 auto;
+  padding: 30px;
+  max-width: 250px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  border: 1px solid ${ p => p.theme.colors.black };
+  border-radius: 4px;
+  background-color: ${ p => p.theme.colors.lightMint };
+  box-shadow: 2px 4px 10px 3px ${ p => p.theme.colors.translucentBlack };
+`
+
+const LabelTag = styled.label`
+  display: block;
+`
+
+const InputTag = styled.input.attrs( { type: "text" } )<IInput>`
+  padding: 2px 4px;
+  border: 1px solid ${ p => p.theme.colors.black };
+  border-radius: 4px;
+`;
+
+const ButtonSubmit = styled.button.attrs( { type: "submit" } )`
+  padding: 2px 4px;
+  border: 1px solid ${ p => p.theme.colors.black };
+  border-radius: 4px;
+  background-color: ${ p => p.theme.colors.paleMint };
+  font-weight: 600;
+  color: ${ p => p.theme.colors.translucentBlackThirty };
+  transition: all 0.3s linear;
+  &:hover {
+    background-color: ${ p => p.theme.colors.mint };
+    color: ${ p => p.theme.colors.translucentBlack };
+  }
+  &:active {
+    background-color: ${ p => p.theme.colors.brightMint };
+    color: ${ p => p.theme.colors.black };
+  }
+`;
+
+const Input = ({
+                          id,
+                          label,
+                          validation,
+                          onChange,
+                          onKeyPress,
+                          value,
+                          placeholder
+                      }: IInput) => {
+
+    const [ inputValue, setInputValue ] = useState( "" );
+
+    const showInputValue = ( e: React.FormEvent<HTMLInputElement> ) => {
+        const { target } = e;
+        const { value } = target as HTMLInputElement;
+        console.log( inputValue );
+        setInputValue( value );
+    };
+
+    const handleChange = ( e: React.FormEvent<HTMLButtonElement> ) => {
+        e.preventDefault();
+    };
+
     return (
-        <></>
+        <>
+            <CommonContainer>
+                <FormTag>
+                    <LabelTag
+                        // for = { id }
+                    >
+                        What's your name ?
+                    </LabelTag>
+                    <InputTag
+                        // id = { id }
+                        onChange = { showInputValue }
+                        onKeyPress = { onKeyPress }
+                        value = { value }
+                        placeholder = "Enter your name"
+                    />
+                    <ButtonSubmit
+                        onClick = { handleChange }
+                    >
+                        submit
+                    </ButtonSubmit>
+                </FormTag>
+            </CommonContainer>
+        </>
     );
 };
 
+export default Input;
+
 // Создать кастомный компонент для поля ввода (Input) , который может принимать в себя следующие пропсы:
-//      id - id для input и соответствующего ему label (string, default: ‘’),
-//      className - класс для стилизации (string, default: ''),
-//      label - label для нашего input (string, default: ''),
+    //      id - id для input и соответствующего ему label (string, default: ‘’),
+    //      label - label для нашего input (string, default: ''),
+
 //      validation - массив объектов вида:
 //          { validationFunc, error }
 //              validationFunc - функция, по которой можно проводить валидацию
