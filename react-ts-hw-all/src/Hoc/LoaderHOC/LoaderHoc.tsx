08e96ffc617ecbab4fa45cc3e0b87b71c15b563e
Zhenya Aleksandrova <laberfo@gmail.com>
2022-04-24 18:30:18 +0300

    fix: changed HOC on button-click in Hoc/LoaderHOC/LoaderHoc.tsx

--- react-ts-hw-all/src/Hoc/LoaderHOC/LoaderHoc.tsx
+++ react-ts-hw-all/src/Hoc/LoaderHOC/LoaderHoc.tsx
@@ -3,83 +3,53 @@ import styled from "styled-components";
 
 interface IWithLoadingProps {
     data: any | undefined | null;
-};
+}
 
 const LoaderWrapper = styled.div`
   width: 120px;
   height: 120px;
-   @keyframes rotate {
+  @keyframes rotate {
     0% {
       transform: scale(1) rotate(360deg);
     }
     50% {
-      transform: scale(.8) rotate(-360deg);
+      transform: scale(0.8) rotate(-360deg);
     }
-  }
+  };
 `;
 
 const Loader = styled.div`
   width: 100%;
   height: 100%;
-  border: 18px solid ${ p => p.theme.colors.veryDarkBlue };
-  border-top-color: ${ p => p.theme.colors.mint };
-  border-bottom-color: ${ p => p.theme.colors.brightBlue };
+  border: 18px solid ${(p) => p.theme.colors.veryDarkBlue};
+  border-top-color: ${(p) => p.theme.colors.mint};
+  border-bottom-color: ${(p) => p.theme.colors.brightBlue};
   border-radius: 50%;
   animation: rotate 5s linear infinite;
   &.small {
-    border-top-color: ${ p => p.theme.colors.white };
-    border-bottom-color: ${ p => p.theme.colors.softPink };
+    border-top-color: ${(p) => p.theme.colors.white};
+    border-bottom-color: ${(p) => p.theme.colors.softPink};
     animation-duration: 2.5s;
   }
 `;
 
-const isEmpty = ( prop: any ) => {
-    return prop === null
-    || prop === undefined
-    || (prop.hasOwnProperty("length") && prop.length === 0)
-    || (prop.constructor === Object && Object.keys(prop).length === 0)
+export const LoaderHOC = (loadingProp: string) => <
+    P extends Record<string, any>
+    >(
+    WrappedComponent: React.ComponentType<P>
+) => {
+    const WithLoading = (props: P & IWithLoadingProps) => {
+        return props?.[loadingProp] ? (
+            <LoaderWrapper>
+                <Loader className="loader">
+                    <Loader className="loader small"></Loader>
+                </Loader>
+            </LoaderWrapper>
+        ) : (
+            <WrappedComponent {...(props as P)} />
+        );
+    };
+    return WithLoading;
 };
 
-export const LoaderHOC = ( loadingProp: string ) =>
-    <P extends Record<string, any>>(WrappedComponent: React.ComponentType<P>) => {
-
-        const WithLoading = ( props: P & IWithLoadingProps ) => {
-            return isEmpty( props?.[ loadingProp ] ) ?
-                <LoaderWrapper>
-                    <Loader className = "loader">
-                        <Loader className = "loader small"></Loader>
-                    </Loader>
-                </LoaderWrapper> :
-                <WrappedComponent {...props as P} />
-        }
-        return WithLoading;
-}
-
 export default LoaderHOC;
-
-
-// just was trying to do loading on button-click
-
-// type TLoading = boolean;
-//
-// const isEmptyAndLoading = ( prop: any, loading: TLoading ) => {
-//     return ( prop === null && loading )
-//         || ( prop === undefined && loading )
-//         || ( (prop.hasOwnProperty("length") && prop.length === 0) && loading )
-//         || ( (prop.constructor === Object && Object.keys(prop).length === 0) && loading)
-// };
-//
-// export const LoaderHOCTwo = ( loadingProp: string ) =>
-//     <P extends Record<string, any>>(WrappedComponent: React.ComponentType<P>) => {
-//
-//         const WithLoading = ( props: P & WithLoadingProps ) => {
-//             return isEmptyAndLoading( props?.[ loadingProp ], props?.isLoading ) ?
-//                 <LoaderWrapper>
-//                     <Loader className = "loader">
-//                         <Loader className = "loader small"></Loader>
-//                     </Loader>
-//                 </LoaderWrapper> :
-//                 <WrappedComponent {...props as P} />
-//         };
-//         return WithLoading;
-//     };
